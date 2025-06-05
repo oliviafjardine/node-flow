183ec1c6b5db5699258ddc57c3b2d29a7ed919d5
oliviafjardine <jardineo@oregonstate.edu>
2025-06-04 23:11:28 -0700

    Update to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
     <section className="relative min-h-[calc(100vh-4rem)] flex items-center justify-center text-center">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <h1 className="text-4xl md:text-5xl font-bold mb-4">
-          Learn Data Structures and Algorithms{" "}
+          Learn Data Structures & Algorithms{" "}
           <span className="text-blue-600">Through Visualization</span>
         </h1>
         <p className="mx-auto s:mx-4 md:mx-20 text-lg text-gray-600">
